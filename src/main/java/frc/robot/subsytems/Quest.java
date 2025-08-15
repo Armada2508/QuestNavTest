f2830dc8faa1145f2bb8ea48902fbb60a5c8cc90
chris <christopp1228@gmail.com>
2025-08-15 11:51:04 -0500

    changes from before that i forgot

---
+++ src/main/java/frc/robot/subsytems/Quest.java
@@ -0,0 +1,57 @@
+package frc.robot.subsytems;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Quaternion;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.QuestNav;
+import frc.robot.Constants.QuestK;
+
+public class Quest extends SubsystemBase {
+     private QuestNav quest = new QuestNav();
+
+    @Override
+    public void periodic() {
+       quest.cleanupResponses();
+       quest.processHeartbeat();
+       quest.setPose(quest.getPose().transformBy(QuestK.questOffset));
+    }
+
+    public Pose2d getRobotPose() {
+        return quest.getPose().transformBy(QuestK.questOffset);
+    }
+
+    public Matrix<N3, N1> getQuestResults() {
+        return null; // fix
+    }
+
+    public boolean isQuestHappy() {
+        return quest.getConnected() && quest.getTrackingStatus();
+    }
+
+    // QUEST ACCESSORS (makes code pretty :3)
+
+    public Quaternion getQuaternion() {
+        return quest.getQuaternion();
+    }
+
+    public double getBatteryPercent() {
+        return quest.getBatteryPercent();
+    }
+
+    public double getTimeStamp() {
+        return quest.getTimestamp();
+    }
+
+    public Long getFrameCount() {
+        return quest.getFrameCount();
+    }
+
+    public Long getTrackingLostCounter() {
+        return quest.getTrackingLostCounter();
+    }
+}
+
+
