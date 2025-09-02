d9f9263c0f227fc6c686a8633357d684c76ec4ad
FarmerOfEggplants <andrewboy1389@gmail.com>
2025-09-02 16:02:28 -0500

    removed duplicate quest

--- src/main/java/frc/robot/subsystems/Quest.java
+++ src/main/java/frc/robot/subsystems/Quest.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Quaternion;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,14 +10,7 @@ import frc.robot.QuestNav;
 import frc.robot.Constants.QuestK;
 
 public class Quest extends SubsystemBase {
-    // public record QuestData(
-        // double batteryPercent,
-        // double timestamp,
-        // Pose3d pos
-        //~ Maybe finish this? see how much we need it
-    // ) {}
-
-     QuestNav quest = new QuestNav();
+     private QuestNav quest = new QuestNav();
 
     @Override
     public void periodic() {
@@ -31,14 +24,34 @@ public class Quest extends SubsystemBase {
     }
 
     public Matrix<N3, N1> getQuestResults() {
-        return null; //! Fix
+        return null; // fix
     }
 
     public boolean isQuestHappy() {
         return quest.getConnected() && quest.getTrackingStatus();
     }
 
-    public Rotation2d getQuestRotation() {
-        return quest.getPose().getRotation();
+    // QUEST ACCESSORS (makes code pretty :3)
+
+    public Quaternion getQuaternion() {
+        return quest.getQuaternion();
+    }
+
+    public double getBatteryPercent() {
+        return quest.getBatteryPercent();
+    }
+
+    public double getTimeStamp() {
+        return quest.getTimestamp();
+    }
+
+    public Long getFrameCount() {
+        return quest.getFrameCount();
+    }
+
+    public Long getTrackingLostCounter() {
+        return quest.getTrackingLostCounter();
     }
 }
+
+
